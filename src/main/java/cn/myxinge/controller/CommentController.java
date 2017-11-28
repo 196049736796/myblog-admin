96fd8a84e338867bb7c480a942b0180a800a61d0
chenxinghua <1960497367@qq.com>
2017-11-28 18:19:33 +0800

    更新留言显示

---
+++ src/main/java/cn/myxinge/controller/CommentController.java
@@ -0,0 +1,29 @@
+package cn.myxinge.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by chenxinghua on 2017/11/28.
+ * 评论处理
+ */
+@RestController
+public class CommentController {
+    @RequestMapping("/saveComment")
+    public void test(@RequestBody String body){
+        System.out.println("----------");
+        System.out.println(body);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
