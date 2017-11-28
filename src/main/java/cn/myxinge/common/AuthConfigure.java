174c8c83d4a20d54aa38f75cdbc6c5ad4a3fd100
Xingchen <1960497367@qq.com>
2017-11-28 22:48:28 +0800

    添加C3P0数据库，前端改变

---
+++ src/main/java/cn/myxinge/common/AuthConfigure.java
@@ -0,0 +1,33 @@
+package cn.myxinge.common;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by XingChen on 2017/11/28.
+ */
+//加上注释@Component，可以直接其他地方使用@Autowired来创建其实例对象
+@Component
+//springboot1.5之前的版本这么用，1.5版本之后取消了locations属性，替代方案看最下面。
+@ConfigurationProperties(prefix = "db")
+@PropertySource("classpath:/db.properties")
+public class AuthConfigure {
+    private String username;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
